e06cd830ac3fd8f70d9798e66cf43c6ac42dec0e
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-08-27 14:27:01 +0100

    set associations between order, user,  cart and product

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -3,11 +3,27 @@ import Product from "./Product.js";
 import User from "./User.js";
 import Comment from "./comment.js";
 import sequelize from "../index.js";
+import Orders from './Orders.js'
+import Cart from './Cart.js'
 
+//relation between product and category
 Product.belongsTo(Category);
 Category.hasMany(Product);
+
+//relation between user, comment and Product
 Comment.belongsTo(User)
 User.hasMany(Comment)
 Product.hasMany(Comment)
+
+//relation between user, order
+Orders.belongsTo(User)
+User.hasMany(Orders)
+
+//relation between Order, Product and cart
+Cart.belongsTo(Orders)
+Orders.hasMany(Cart,{ onDelete: "cascade",})
+
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
 
 export default { Category, sequelize, Product,User,Comment };
